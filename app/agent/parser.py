f99ed6135f14ec8401429823b85f942ce0747573
Priyanshu Khandelwal <p.h.khandelwal@accenture.com>
2025-12-26 06:29:38 +0530

    CI-CD check

--- app/agent/parser.py
+++ app/agent/parser.py
@@ -2,6 +2,7 @@ from langchain_core.output_parsers import PydanticOutputParser
 from app.agent.schema import ExtractedData
 
 def get_output_parser() -> PydanticOutputParser:
+    
     """
     Return the PydanticOutputParser with the required Schema
     """
